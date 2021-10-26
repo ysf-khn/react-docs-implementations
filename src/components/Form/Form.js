fdc83406053caf4680cf420ede590272ced5eacd
Yusuf <yusufmohd72@gmail.com>
2021-10-26 20:40:43 +0530

    Form events

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,18 +1,30 @@
 import { useState } from "react";
 
 const Form = () => {
-  const [value, setValue] = useState("Enter");
+  const [value, setValue] = useState("");
+  const [text, setText] = useState("");
 
-  const alertUser = () => {
-    alert("Submitted");
+  const alertUser = (e) => {
+    alert(`Submitted ${e.target.value}`);
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const logValue = (e) => {
+    setText(e.target.value);
+    console.log(text + e.target.value);
   };
 
   return (
     <>
       <form onSubmit={alertUser}>
         <label> Name :</label>
-        <input type="text" value={value} />
-        <input type="submit" value="Submit" />
+        <input type="text" value={value} onChange={handleChange} />
+        <input type="submit" value="Submit" onClick={alertUser} />
+        <textarea value={text} onChange={logValue} />
+        <div>{text}</div>
       </form>
     </>
   );
